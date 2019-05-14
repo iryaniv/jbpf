7f9d659a45997ec4f1658398c67b3aa5d6869883
Yaniv Ironi <iryaniv@gmail.com>
2019-05-14 15:53:42 +0300

    Added compiled jbpf.jar

--- jbpf/src/com/iryaniv/jbpf/JBpf.java
+++ jbpf/src/com/iryaniv/jbpf/JBpf.java
@@ -1,10 +1,12 @@
+package com.iryaniv.jbpf;
+
 class JBpf {
     private native int validate(String filter, byte[] packet);
 
     public boolean checkBpf(String filter, byte[] packet) {
         return (validate(filter, packet) == 1);
     }
-    
+
     static {
         System.loadLibrary("Bpf");
     }
