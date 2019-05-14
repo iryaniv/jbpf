ea0ee393f6eec9d5b51208fe6c1d9e7c02f3dc66
Yaniv Ironi <iryaniv@gmail.com>
2019-05-14 15:27:06 +0300

    Added java api to the c liBpf.so

---
+++ jbpf/src/JBpf.java
@@ -0,0 +1,12 @@
+class JBpf {
+    private native int validate(String filter, byte[] packet);
+
+    public boolean checkBpf(String filter, byte[] packet) {
+        return (validate(filter, packet) == 1);
+    }
+    
+    static {
+        System.loadLibrary("Bpf");
+    }
+}
+
